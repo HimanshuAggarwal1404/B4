15d6e57908bde7443bbcf21a0e99dfaf72c08466
Himanshu Aggarwal <120503620+HimanshuAggarwal1404@users.noreply.github.com>
2023-03-08 13:26:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+total = prompt("Enter total number:");
+console.log(total);
+let i = 0;
+
+onepage= prompt("enter no on one page")
+let width;
+width= (100/onepage)
+console.log(width)
+for (i=0; i < total; i++) {
+  var div = document.createElement("div");
+  div.style.width = width+ "vw";
+  div.style.height = "200px";
+  div.style.background = "red";
+  div.style.display = "flex";
+  div.style.justifyContent = "center";
+  div.style.alignItems = "center";
+  div.style.color = "white";
+  div.innerHTML = "Hello";
+  div.style.margin='2px'
+  div.id = i;
+  console.log(i);
+  document.getElementById("content").appendChild(div);
+  
+}
